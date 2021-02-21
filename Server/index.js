4ddae3df7b7b61fd9be7e6eaaab13696aacf91b4
Madan-444 <mmadan3600@gmail.com>
2021-02-21 20:47:26 +0545

    3rd commit

--- Server/index.js
+++ Server/index.js
@@ -13,8 +13,8 @@ const authRoutes = require('./routes/auth')
 app.use(morgan('dev'))
 app.use(bodyParser.json());
 // app.use(cors()) // allows all origins
-if((process.env.NODE_ENV) == 'development') {
-    app.use(cors({origin: `http://localhost:3000`}));
+if((process.env.NODE_ENV) = 'development') {
+    app.use(cors({origin: `${process.env.CLIENT_URL}`}));
 }
 
 //middleware
@@ -22,5 +22,6 @@ app.use('/api',authRoutes)
 const port = process.env.PORT || 8000
 
 app.listen(port, ()=> {
-    console.log(`Your app is running at ${port}`)
+    console.log(`Your app is running at ${port} - ${process.env.Node_ENV}`)
+
 })
